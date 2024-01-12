360552f00e35c30be66f53f75c69460401d68420
Jem <0x0xjem@gmail.com>
2024-01-12 12:47:35 +0400

    Documentation TODOs

--- src/interfaces/IHooks.sol
+++ src/interfaces/IHooks.sol
@@ -5,11 +5,14 @@ pragma solidity >=0.8.0;
 /// @notice Interface for hook contracts to be called during auction payment and payout
 interface IHooks {
     /// @notice Called before payment and payout
+    /// TODO define expected state, invariants
     function pre(uint256 lotId_, uint256 amount_) external;
 
     /// @notice Called after payment and before payout
+    /// TODO define expected state, invariants
     function mid(uint256 lotId_, uint256 amount_, uint256 payout_) external;
 
     /// @notice Called after payment and after payout
+    /// TODO define expected state, invariants
     function post(uint256 lotId_, uint256 payout_) external;
 }
