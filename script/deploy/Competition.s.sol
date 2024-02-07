bbc0ec546b1673583c68385b136c80fbbda702ac
Jem <0x0xjem@gmail.com>
2024-02-07 17:44:58 +0400

    Prep deployment script for use with Permit2 and LinearVesting

--- script/deploy/Competition.s.sol
+++ script/deploy/Competition.s.sol
@@ -4,18 +4,28 @@ pragma solidity 0.8.19;
 // Scripting libraries
 import {Script, console2} from "forge-std/Script.sol";
 
+// Test contracts
+import {Permit2Clone} from "test/lib/permit2/Permit2Clone.sol";
+
 // System contracts
 import {AuctionHouse} from "src/AuctionHouse.sol";
 import {LocalSealedBidBatchAuction} from "src/modules/auctions/LSBBA/LSBBA.sol";
+import {LinearVesting} from "src/modules/derivatives/LinearVesting.sol";
 
 contract CompetitionDeploy is Script {
     AuctionHouse public auctionHouse;
     LocalSealedBidBatchAuction public lsbba;
+    LinearVesting public linearVesting;
+    Permit2Clone public permit2Clone;
 
     function deploy() public {
         vm.startBroadcast();
 
-        auctionHouse = new AuctionHouse(msg.sender, address(0));
+        // Only needed on Blast testnet, since there isn't a Permit2 deployed
+        permit2Clone = new Permit2Clone();
+        console2.log("Permit2Clone deployed at: ", address(permit2Clone));
+
+        auctionHouse = new AuctionHouse(msg.sender, address(permit2Clone));
         console2.log("AuctionHouse deployed at: ", address(auctionHouse));
 
         lsbba = new LocalSealedBidBatchAuction(address(auctionHouse));
@@ -23,6 +33,12 @@ contract CompetitionDeploy is Script {
 
         auctionHouse.installModule(lsbba);
         console2.log("LocalSealedBidBatchAuction installed at AuctionHouse");
+
+        linearVesting = new LinearVesting(address(auctionHouse));
+        console2.log("LinearVesting deployed at: ", address(linearVesting));
+
+        auctionHouse.installModule(linearVesting);
+        console2.log("LinearVesting installed at AuctionHouse");
 
         vm.stopBroadcast();
     }
