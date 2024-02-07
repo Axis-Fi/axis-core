17f5107f73c7fb53b5f3fe87306d15b9f73de7dc
Oighty <oighty6@gmail.com>
2024-02-07 09:52:17 -0600

    script: add Catalogue to deploy script

--- script/deploy/Competition.s.sol
+++ script/deploy/Competition.s.sol
@@ -9,11 +9,13 @@ import {Permit2Clone} from "test/lib/permit2/Permit2Clone.sol";
 
 // System contracts
 import {AuctionHouse} from "src/AuctionHouse.sol";
+import {Catalogue} from "src/Catalogue.sol";
 import {LocalSealedBidBatchAuction} from "src/modules/auctions/LSBBA/LSBBA.sol";
 import {LinearVesting} from "src/modules/derivatives/LinearVesting.sol";
 
 contract CompetitionDeploy is Script {
     AuctionHouse public auctionHouse;
+    Catalogue public catalogue;
     LocalSealedBidBatchAuction public lsbba;
     LinearVesting public linearVesting;
     Permit2Clone public permit2Clone;
@@ -27,6 +29,9 @@ contract CompetitionDeploy is Script {
 
         auctionHouse = new AuctionHouse(msg.sender, address(permit2Clone));
         console2.log("AuctionHouse deployed at: ", address(auctionHouse));
+
+        catalogue = new Catalogue(address(auctionHouse));
+        console2.log("Catalogue deployed at: ", address(catalogue));
 
         lsbba = new LocalSealedBidBatchAuction(address(auctionHouse));
         console2.log("LocalSealedBidBatchAuction deployed at: ", address(lsbba));
