1ae96737f2f459977bf74b9735dcb1174c4c2115
Oighty <oighty6@gmail.com>
2024-02-07 14:04:24 -0600

    refactor: remove extraneous variable

--- src/AuctionHouse.sol
+++ src/AuctionHouse.sol
@@ -215,11 +215,7 @@ contract AuctionHouse is Auctioneer, Router, FeeManager {
 
         // Send purchase to auction house and get payout plus any extra output
         bytes memory auctionOutput;
-        {
-            AuctionModule module = getModuleForId(params_.lotId);
-            (payoutAmount, auctionOutput) =
-                module.purchase(params_.lotId, amountLessFees, params_.auctionData);
-        }
+        (payoutAmount, auctionOutput) = getModuleForId(params_.lotId).purchase(params_.lotId, amountLessFees, params_.auctionData);
 
         // Check that payout is at least minimum amount out
         // @dev Moved the slippage check from the auction to the AuctionHouse to allow different routing and purchase logic
