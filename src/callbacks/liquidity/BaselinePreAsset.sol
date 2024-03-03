2b6c3f2f3aa4cb6b99c2b1015bb1cd180b9bfccb
Oighty <oighty6@gmail.com>
2024-03-03 09:46:16 -0700

    feat: add claim fn and pool deployment to baseline preasset

--- src/callbacks/liquidity/BaselinePreAsset.sol
+++ src/callbacks/liquidity/BaselinePreAsset.sol
@@ -9,10 +9,30 @@ interface IPreAsset {
     function seed() external returns (uint256);
 
     function setBAsset(address bAsset_) external;
+
+    function claim() external returns (uint256);
 }
 
 interface IBAsset {
     function baseline() external view returns (address);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+interface IBaselineFactory {
+    function deploy(
+        address preAsset_,
+        string memory name_,
+        string memory symbol_,
+        address reserve_,
+        bytes32 salt_,
+        address feeRecipient_,
+        int24 initTick_,
+        uint256 initFloor_,
+        uint256 initDisc_
+    ) external returns (address baseline);
 }
 
 /// @notice This contract is a replacement PreAsset for a Baseline Market that allows depositing proceeds from a Batch auction directly into Baseline.
@@ -28,45 +48,47 @@ contract BaselinePreAsset is ERC20, BaseCallback, IPreAsset {
     error PreAsset_BAssetAlreadySet();
     error PreAsset_BAssetNotSet();
     error PreAsset_NotAuthorized();
+    error PreAsset_PoolNotSeeded();
     
 
     // ========== EVENTS ========== //
 
     event Seeded(uint256 totalReserves);
+    event Claim(address indexed user, uint256 amount);
 
     // ========== STATE VARIABLES ========== //
 
     // Baseline Variables
     ERC20 public immutable reserve;
-    address public immutable baselineFactory;
+    IBaselineFactory public immutable baselineFactory;
     IBAsset public bAsset;
 
     /// @notice Lot ID of the auction for the baseline market. This callback only supports one lot.
     uint96 public lotId;
     bool public auctionComplete;
     bool public seeded;
-    uint256 public capacity;
-    uint256 public clearingPrice; // can be used to determine the init parameters of the baseline market
+    uint256 public totalPreAssets;
+    uint256 public totalBAssets;
 
     // ========== CONSTRUCTOR ========== //
 
     constructor(
-        string memory name_,
-        string memory symbol_,
+        string memory preAssetName_,
+        string memory preAssetSymbol_,
         address auctionHouse_,
         Callbacks.Permissions memory permissions_,
         address seller_,
         address reserve_,
         address baselineFactory_
-    ) ERC20(name_, symbol_, 18) BaseCallback(auctionHouse_, permissions_, seller_) {
+    ) ERC20(preAssetName_, preAssetSymbol_, 18) BaseCallback(auctionHouse_, permissions_, seller_) {
         // Check that the reserve token has 18 decimals
         if (ERC20(reserve_).decimals() != 18) revert Callback_InvalidParams();
 
-        // set immutable variables
+        // Store immutable variables
         reserve = ERC20(reserve_);
-        baselineFactory = baselineFactory_;
+        baselineFactory = IBaselineFactory(baselineFactory_);
 
-        // set lot ID to max uint(96) initially
+        // Set lot ID to max uint(96) initially
         lotId = type(uint96).max;
     }
 
@@ -105,9 +127,6 @@ contract BaselinePreAsset is ERC20, BaseCallback, IPreAsset {
         // Set the lot ID
         lotId = lotId_;
 
-        // Store the capacity
-        capacity = uint256(capacity_);
-
         // Mint the capacity of preAsset tokens to the auction house
         _mint(msg.sender, capacity_);
     }
@@ -141,9 +160,6 @@ contract BaselinePreAsset is ERC20, BaseCallback, IPreAsset {
         // Validate that prefund is true
         if (!prefund_) revert Callback_InvalidParams();
 
-        // Update the capacity with the curator fee
-        capacity += curatorFee_;
-
         // Mint the curator fee amount of preAsset tokens to the auction house
         _mint(msg.sender, curatorFee_);
     }
@@ -175,7 +191,7 @@ contract BaselinePreAsset is ERC20, BaseCallback, IPreAsset {
         uint96 lotId_,
         uint96 proceeds_,
         uint96 refund_,
-        bytes calldata,
+        bytes calldata callbackData,
         bytes memory
     ) internal override {
         // Validate the lot ID
@@ -187,14 +203,44 @@ contract BaselinePreAsset is ERC20, BaseCallback, IPreAsset {
         // Validate that the callback received the correct amount of proceeds
         if (proceeds_ < reserve.balanceOf(address(this))) revert Callback_MissingFunds();
 
+        // Decode callback data to get bAsset initialization parameters
+        (string memory name, string memory symbol, bytes32 salt, address feeRecipient) = abi.decode(callbackData, (string, string, bytes32, address));
+
         // Set the auction as complete
         auctionComplete = true;
 
-        // Calculate the clearing price in quote tokens per base token
-        clearingPrice = (proceeds_ * 1e18) / (capacity - refund_);
-
         // Burn any refunded preAsset tokens that were sent from the auction house
         _burn(address(this), refund_);
+
+        // Store the total supply of preAsset tokens at the point of baseline deployment
+        totalPreAssets = totalSupply;
+
+        // Calculate the clearing price in quote tokens per base token
+        // TODO discuss with baseline team
+        uint256 clearingPrice = (proceeds_ * 1e18) / totalPreAssets;
+        int24 initTick = 0; // TODO calculate from clearing price
+        uint256 initFloor = 0; // TODO liquidity in floor
+        uint256 initDisc = 0; // TODO liquidity in discovery
+
+        
+        // Deploy the baseline pool
+        // This function will re-enter this contract to set the bAsset and seed the pool
+        // At the end of the call, this contract should have the initial supply of bAssets
+        // We store them to use for claiming
+        baselineFactory.deploy(
+            address(this),
+            name,
+            symbol,
+            address(reserve),
+            salt,
+            feeRecipient,
+            initTick,
+            initFloor,
+            initDisc
+        );
+
+        // Store the total bAssets received
+        totalBAssets = bAsset.balanceOf(address(this));
     }
 
     // ========== BASELINE PREASSET FUNCTIONS ========== //
@@ -228,13 +274,33 @@ contract BaselinePreAsset is ERC20, BaseCallback, IPreAsset {
     // Set the precalculated BAsset address, must be called before baseline deployment
     function setBAsset(address bAsset_) external override {
         // Ensure caller is the baseline factory
-        if (msg.sender != baselineFactory) revert PreAsset_NotAuthorized();
+        if (msg.sender != address(baselineFactory)) revert PreAsset_NotAuthorized();
 
         // Ensure the bAsset has not already been set
         if (address(bAsset) != address(0)) revert PreAsset_BAssetAlreadySet();
 
         // Set the bAsset
         bAsset = IBAsset(bAsset_);
+    }
+
+    // Claim bAssets
+    function claim() external returns (uint256) {
+        // Validate that pool has been seeded and the preasset received the bAssets
+        if (!seeded) revert PreAsset_PoolNotSeeded();
+
+        // Get the user's balance of preAsset tokens
+        uint256 preAssetBalance = balanceOf[msg.sender];
+
+        // If balance is 0, return 0
+        if (preAssetBalance == 0) return 0;
+
+        // Calculate amount claimable
+        uint256 claimable = (preAssetBalance * totalBAssets) / totalPreAssets;
+
+        bAsset.transfer(msg.sender, claimable);
+
+        emit Claim(msg.sender, claimable);
+        return claimable;
     }
 
 }
