2db8324b3c75cf9f19430c90ead93984fcc02ab8
Jem <0x0xjem@gmail.com>
2024-03-12 12:55:20 +0400

    Remove non-prefunding tests for ClaimProceeds

--- test/AuctionHouse/AuctionHouseTest.sol
+++ test/AuctionHouse/AuctionHouseTest.sol
@@ -473,11 +473,6 @@ abstract contract AuctionHouseTest is Test, Permit2User {
         _;
     }
 
-    modifier givenBatchAuctionRequiresPrefunding() {
-        _batchAuctionModule.setRequiredPrefunding(true);
-        _;
-    }
-
     modifier givenLotProceedsAreClaimed() {
         vm.prank(_SELLER);
         _auctionHouse.claimProceeds(_lotId);
