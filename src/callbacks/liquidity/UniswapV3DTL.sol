05598e07ef0699a8a9a6ec5ae25e1c7f5ab24abc
Jem <0x0xjem@gmail.com>
2024-03-19 14:07:26 +0400

    Structs and variables

--- src/callbacks/liquidity/UniswapV3DTL.sol
+++ src/callbacks/liquidity/UniswapV3DTL.sol
@@ -12,15 +12,61 @@ import {Callbacks} from "src/lib/Callbacks.sol";
 contract UniswapV3DirectToLiquidity is BaseCallback {
     // ========== ERRORS ========== //
 
+    // ========== STRUCTS ========== //
+
+    /// @notice     Configuration for the DTL callback
+    struct DTLConfiguration {
+        address baseToken;
+        address quoteToken;
+        uint96 lotCapacity;
+        uint24 proceedsUtilisationPercent;
+        uint24 poolFee;
+        int24 poolTickLower;
+        int24 poolTickUpper;
+        uint48 vestingStart;
+        uint48 vestingExpiry;
+    }
+
+    /// @notice     Parameters used in the onCreate callback
+    ///
+    /// @param      proceedsUtilisationPercent   The percentage of the proceeds to use in the pool
+    /// @param      poolFee                      The Uniswap V3 fee tier for the pool
+    /// @param      poolTickLower                The lower tick of the Uniswap V3 pool
+    /// @param      poolTickUpper                The upper tick of the Uniswap V3 pool
+    /// @param      vestingStart                 The start of the vesting period for the LP tokens (0 if disabled)
+    /// @param      vestingExpiry                The end of the vesting period for the LP tokens (0 if disabled)
+    struct DTLParams {
+        uint24 proceedsUtilisationPercent;
+        uint24 poolFee;
+        int24 poolTickLower;
+        int24 poolTickUpper;
+        uint48 vestingStart;
+        uint48 vestingExpiry;
+    }
+
     // ========== STATE VARIABLES ========== //
 
-    // TODO determine structure for storing pool configuration and vesting information
+    uint24 public constant MAX_PERCENT = 1e5;
+
+    /// @notice     Maps the lot id to the DTL configuration
+    mapping(uint96 lotId => DTLConfiguration) public lotConfiguration;
 
     constructor(
         address auctionHouse_,
         Callbacks.Permissions memory permissions_,
         address seller_
-    ) BaseCallback(auctionHouse_, permissions_, seller_) {}
+    ) BaseCallback(auctionHouse_, permissions_, seller_) {
+        // Ensure that the required permissions are met
+        if (
+            !permissions_.onCreate || !permissions_.onCancel || !permissions_.onCurate
+                || !permissions_.onClaimProceeds || !permissions_.receiveQuoteTokens
+        ) {
+            revert Callback_InvalidParams();
+        }
+    }
+
+    // [ ] Consider using bunni to manage the pool tokens
+    // [ ] Functions to handle vesting LP tokens. Can we reuse LinearVesting?
 
     // ========== CALLBACK FUNCTIONS ========== //
 
@@ -30,16 +76,32 @@ contract UniswapV3DirectToLiquidity is BaseCallback {
         address baseToken_,
         address quoteToken_,
         uint96 capacity_,
-        bool,
+        bool prefund_,
         bytes calldata callbackData_
     ) internal virtual override {
-        // TODO
-        // Decode pool configuration and vesting information from callback data
-        // [ ] - Percent of proceeds to use in pool
-        // [ ] - Duration to vest the LP tokens
-        // [ ] - Pool specific info (e.g. fee tier, tick spacing, etc.)
-        // Use baseToken and quoteToken as assets in new pool
-        // Store capacity of market (should have that amount extra in this contract to pair)
+        // Decode callback data into the params
+        // TODO check length of callbackData to provide a more helpful error message
+        DTLParams memory params = abi.decode(callbackData_, (DTLParams));
+
+        // Validate the parameters
+        // utilisation within range (> 0, < MAX_PERCENT)
+        // pool fee
+        // ticks: lower < upper, within range
+        // vesting start < expiry (use LinearVesting.validate()?)
+        // if sending base tokens, should have the capacity of base token to create the pool
+
+        // Store the configuration
+        lotConfiguration[lotId_] = DTLConfiguration({
+            baseToken: baseToken_,
+            quoteToken: quoteToken_,
+            lotCapacity: capacity_,
+            proceedsUtilisationPercent: params.proceedsUtilisationPercent,
+            poolFee: params.poolFee,
+            poolTickLower: params.poolTickLower,
+            poolTickUpper: params.poolTickUpper,
+            vestingStart: params.vestingStart,
+            vestingExpiry: params.vestingExpiry
+        });
 
         // Should we assume the callback is sending base tokens? Account for both cases?
         // Assume that the auction is prefunded since you should only create this with a claimProceeds callback
@@ -80,6 +142,8 @@ contract UniswapV3DirectToLiquidity is BaseCallback {
         // TODO
         // Reduce expected tokens to deposit by the refund
         // Create pool and add proceeds and paired tokens to the pool
+
+        // TODO how to determine the ratio between base and quote tokens?
 
         // Conditionally, create vesting tokens with the received LP tokens
         // Send spot or vesting LP tokens to seller
