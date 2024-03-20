a2ddb76acc3c92e09fab081ec648894e8fcb3948
Jem <0x0xjem@gmail.com>
2024-03-20 17:09:26 +0400

    Messing around with sqrtPriceX96

--- test/lib/SqrtPriceMath.t.sol
+++ test/lib/SqrtPriceMath.t.sol
@@ -5,15 +5,19 @@ import {Test} from "forge-std/Test.sol";
 
 import {SqrtPriceMath} from "src/lib/uniswap-v3/SqrtPriceMath.sol";
 
+import {TickMath} from "uniswap-v3-core/libraries/TickMath.sol";
+
+import {console2} from "forge-std/console2.sol";
+
 contract SqrtPriceMathTest is Test {
 
     address internal constant TOKEN0 = address(0x1);
     address internal constant TOKEN1 = address(0x2);
 
     // From DAI-WETH pool
-    uint256 internal constant AMOUNT0 = 3354644702045819714221889; // DAI
-    uint256 internal constant AMOUNT1 = 1128870660210923587702; // WETH
-    uint160 internal constant SQRTPRICEX96 = 1369947724019237678865797100;
+    uint256 internal constant AMOUNT0 = 100e18 * 3344; // DAI
+    uint256 internal constant AMOUNT1 = 100e18; // WETH
+    uint160 internal constant SQRTPRICEX96 = 1369947724019237678865797100; // sqrt(amount1/amount0)*2^96
 
     // [X] when tokenA is token0
     //  [X] it calculates the correct sqrtPriceX96
@@ -27,6 +31,8 @@ contract SqrtPriceMathTest is Test {
     function test_whenTokenAIsToken0() public {
         uint160 sqrtPriceX96 = SqrtPriceMath.getSqrtPriceX96(TOKEN0, TOKEN1, AMOUNT0, AMOUNT1);
         assertEq(sqrtPriceX96, SQRTPRICEX96, "SqrtPriceX96");
+
+        console2.log("ratio", TickMath.getSqrtRatioAtTick(-81152));
     }
 
     function test_whenTokenAIsToken1() public {
