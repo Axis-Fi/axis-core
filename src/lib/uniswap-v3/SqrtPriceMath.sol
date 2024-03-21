ebbb3d0bed34e82b6093ced99cdc96d60c54e603
Jem <0x0xjem@gmail.com>
2024-03-21 12:03:00 +0400

    Correct inputs for SqrtPriceMath tests. Debug logs.

--- src/lib/uniswap-v3/SqrtPriceMath.sol
+++ src/lib/uniswap-v3/SqrtPriceMath.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.19;
 
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
+import {console2} from "forge-std/console2.sol";
+
 library SqrtPriceMath {
     function getSqrtPriceX96(
         address tokenA,
@@ -14,10 +16,16 @@ library SqrtPriceMath {
             tokenA < tokenB ? (amountA, amountB) : (amountB, amountA);
 
         // Source: https://github.com/Uniswap/v3-sdk/blob/2c8aa3a653831c6b9e842e810f5394a5b5ed937f/src/utils/encodeSqrtRatioX96.ts
-        // Needs to be verified. Does it need to handle token decimal differences?
+        // SqrtPriceX96 = sqrt(amount1/amount0) * 2^96
+        //              = sqrt(amount1 * 2^192 / amount0)
+
+        // This seems to be truncating 185339349000000000000000 * 2^192 from the expected value of 1.1633939492e81 to 30828676021860619530268216106975167770139507879063629246398176496786507563008 (3.0828676022e76)
         uint256 numerator = amount1 << 192;
+        console2.log("numerator", numerator);
         uint256 denominator = amount0;
-        uint256 ratioX192 = (numerator / denominator);
+        console2.log("denominator", denominator);
+        uint256 ratioX192 = (numerator / denominator) << 64;
+        console2.log("ratioX192", ratioX192);
         uint256 sqrtPriceX96Temp = FixedPointMathLib.sqrt(ratioX192);
 
         // TODO determine if this is the correct course of action - it would brick claiming proceeds
--- test/lib/SqrtPriceMath.t.sol
+++ test/lib/SqrtPriceMath.t.sol
@@ -10,30 +10,32 @@ import {TickMath} from "uniswap-v3-core/libraries/TickMath.sol";
 import {console2} from "forge-std/console2.sol";
 
 contract SqrtPriceMathTest is Test {
-
     address internal constant TOKEN0 = address(0x1);
     address internal constant TOKEN1 = address(0x2);
 
-    // From DAI-WETH pool
-    uint256 internal constant AMOUNT0 = 100e18 * 3344; // DAI
-    uint256 internal constant AMOUNT1 = 100e18; // WETH
-    uint160 internal constant SQRTPRICEX96 = 1369947724019237678865797100; // sqrt(amount1/amount0)*2^96
-    // TODO the reserves are for all positions whereas sqrtPriceX96 above is for the _current_ tick, so they don't match. Need values from a full-range position, perhaps. Use OHM-WETH?
+    // From OHM-WETH pool
+    // Token id: 562564
+    // https://revert.finance/#/account/0x245cc372C84B3645Bf0Ffe6538620B04a217988B (for current amounts)
+    // https://etherscan.io/address/0x88051b0eea095007d3bef21ab287be961f3d8598#readContract#F11 (for sqrtPriceX96)
+    uint256 internal constant AMOUNT0 = 647_257_004_000_000_000_000; // OHM
+    uint256 internal constant AMOUNT1 = 185_339_349_000_000_000_000_000; // WETH
+    uint160 internal constant SQRTPRICEX96 = 148_058_773_132_005_407_513_152_397_312_640;
+    // sqrt((647.257004*1e18)/(185339.349*1e9))*2**96 ~= 148058773132005407513152397312640
 
     // [X] when tokenA is token0
     //  [X] it calculates the correct sqrtPriceX96
     // [X] when tokenA is token1
     //  [X] it calculates the correct sqrtPriceX96
-    // [ ] when tokenA decimals is greater than tokenB decimals
-    //  [ ] it calculates the correct sqrtPriceX96
-    // [ ] when tokenA decimals is less than tokenB decimals
-    //  [ ] it calculates the correct sqrtPriceX96
+    // [X] when tokenA decimals is greater than tokenB decimals
+    //  [X] it calculates the correct sqrtPriceX96
+    // [X] when tokenA decimals is less than tokenB decimals
+    //  [X] it calculates the correct sqrtPriceX96
 
     function test_whenTokenAIsToken0() public {
         uint160 sqrtPriceX96 = SqrtPriceMath.getSqrtPriceX96(TOKEN0, TOKEN1, AMOUNT0, AMOUNT1);
         assertEq(sqrtPriceX96, SQRTPRICEX96, "SqrtPriceX96");
 
-        console2.log("ratio", TickMath.getSqrtRatioAtTick(-81152));
+        console2.log("ratio", TickMath.getSqrtRatioAtTick(-81_152));
     }
 
     function test_whenTokenAIsToken1() public {
