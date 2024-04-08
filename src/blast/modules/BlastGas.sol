7ea4b95176710dc6aa4542bc96bf65c79e86698a
Oighty <oighty6@gmail.com>
2024-04-08 13:00:20 -0500

    fix: call configureClaimableGas on blast modules

--- src/blast/modules/BlastGas.sol
+++ src/blast/modules/BlastGas.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.19;
 
 interface IBlast {
+    function configureClaimableGas() external;
+
     function configureGovernor(address governor_) external;
 }
 
@@ -9,6 +11,8 @@ abstract contract BlastGas {
     // ========== CONSTRUCTOR ========== //
 
     constructor(address parent_) {
+        // Configure gas as claimable
+        IBlast(0x4300000000000000000000000000000000000002).configureClaimableGas();
         // Configure governor to claim gas fees
         IBlast(0x4300000000000000000000000000000000000002).configureGovernor(parent_);
     }
