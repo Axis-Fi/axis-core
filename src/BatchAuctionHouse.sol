5a8a1fdb8eb99c20f0837af06ba6f25f18486e5b
Oighty <oighty6@gmail.com>
2024-04-10 11:23:44 -0500

    refactor: port settle changes over from #119 with some add'l updates

--- src/BatchAuctionHouse.sol
+++ src/BatchAuctionHouse.sol
@@ -441,6 +441,8 @@ contract BatchAuctionHouse is AuctionHouse, BatchRouter {
         (uint256 purchased_, uint256 sold_, uint256 capacity_) =
             _getBatchModuleForId(lotId_).claimProceeds(lotId_);
 
+
+
         // Load data for the lot
         Routing storage routing = lotRouting[lotId_];
         FeeData storage feeData = lotFees[lotId_];
