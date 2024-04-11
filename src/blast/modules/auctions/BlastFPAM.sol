012fca42f2480369ee0f8db94921d588b17d2af6
Oighty <oighty6@gmail.com>
2024-04-11 10:51:10 -0500

    feat: add blast FPAM

---
+++ src/blast/modules/auctions/BlastFPAM.sol
@@ -0,0 +1,14 @@
+/// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.19;
+
+import {FixedPriceAuctionModule} from "src/modules/auctions/FPAM.sol";
+import {BlastGas} from "src/blast/modules/BlastGas.sol";
+
+contract BlastFPAM is FixedPriceAuctionModule, BlastGas {
+    // ========== CONSTRUCTOR ========== //
+
+    constructor(
+        address auctionHouse_,
+        address blast_
+    ) FixedPriceAuctionModule(auctionHouse_) BlastGas(auctionHouse_, blast_) {}
+}
