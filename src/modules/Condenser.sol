36ec995b17094f7a238a682a60cfde9bb8985de6
Jem <0x0xjem@gmail.com>
2024-05-10 11:44:12 +0400

    NatSpec for Condenser

--- src/modules/Condenser.sol
+++ src/modules/Condenser.sol
@@ -5,4 +5,7 @@ import {ICondenser} from "src/interfaces/ICondenser.sol";
 
 import {Module} from "src/modules/Modules.sol";
 
+/// @title  CondenserModule
+/// @notice The CondenserModule contract is an abstract contract that provides condenser functionality for the AuctionHouse.
+/// @dev    This contract is intended to be inherited by condenser modules that are used in the AuctionHouse.
 abstract contract CondenserModule is ICondenser, Module {}
