84c64fa7f2112f061202cfd1a301ee15254074fa
Jem <0x0xjem@gmail.com>
2024-05-20 15:33:13 +0400

    FPB: decimal tests for bid creation

--- test/modules/auctions/FPB/bid.t.sol
+++ test/modules/auctions/FPB/bid.t.sol
@@ -37,6 +37,10 @@ contract FpbBidTest is FpbTest {
     //  [X] it records the bid and concludes the auction
     // [X] when the bid amount is greater than the remaining capacity
     //  [X] it records the bid, concludes the auction and calculates partial fill
+    // [X] when the quote token decimals are greater
+    //  [X] it records the bid accurately
+    // [X] when the quote token decimals are smaller
+    //  [X] it records the bid accurately
     // [X] it records the bid
 
     function test_notParent_reverts() public {
@@ -194,6 +198,116 @@ contract FpbBidTest is FpbTest {
         assertEq(partialFill.payout, 0, "partialFill: payout");
     }
 
+    function test_bidsReachCapacity_quoteTokenDecimalsLarger()
+        public
+        givenQuoteTokenDecimals(21)
+        givenBaseTokenDecimals(13)
+        givenLotIsCreated
+        givenLotHasStarted
+    {
+        // Create a bid to fill half capacity (10/2 = 5)
+        _createBid(_scaleQuoteTokenAmount(10e18));
+
+        // Create a second bid to fill the remaining capacity exactly
+        _createBid(_scaleQuoteTokenAmount(10e18));
+
+        // Assert state
+        IAuction.Lot memory lotData = _module.getLot(_lotId);
+        assertEq(lotData.capacity, _scaleBaseTokenAmount(_LOT_CAPACITY), "capacity");
+        assertEq(lotData.conclusion, uint48(block.timestamp), "conclusion");
+
+        IFixedPriceBatch.AuctionData memory auctionData = _module.getAuctionData(_lotId);
+        assertEq(uint8(auctionData.status), uint8(IFixedPriceBatch.LotStatus.Created), "status");
+        assertEq(auctionData.nextBidId, 3, "nextBidId");
+        assertEq(auctionData.settlementCleared, false, "settlementCleared"); // Not settled yet
+        assertEq(auctionData.totalBidAmount, _scaleQuoteTokenAmount(20e18), "totalBidAmount");
+
+        // Assert bid one
+        IFixedPriceBatch.Bid memory bidData = _module.getBid(_lotId, 1);
+        assertEq(bidData.bidder, _BIDDER, "bid one: bidder");
+        assertEq(bidData.referrer, _REFERRER, "bid one: referrer");
+        assertEq(bidData.amount, _scaleQuoteTokenAmount(10e18), "bid one: amount");
+        assertEq(
+            uint8(bidData.status), uint8(IFixedPriceBatch.BidStatus.Submitted), "bid one: status"
+        );
+
+        // Assert bid two
+        bidData = _module.getBid(_lotId, 2);
+        assertEq(bidData.bidder, _BIDDER, "bid two: bidder");
+        assertEq(bidData.referrer, _REFERRER, "bid two: referrer");
+        assertEq(bidData.amount, _scaleQuoteTokenAmount(10e18), "bid two: amount");
+        assertEq(
+            uint8(bidData.status), uint8(IFixedPriceBatch.BidStatus.Submitted), "bid two: status"
+        );
+
+        // Settle the auction
+        _warpAfterSettlePeriod();
+        _settleLot();
+
+        // Assert partial fill
+        (bool hasPartialFill, IFixedPriceBatch.PartialFill memory partialFill) =
+            _module.getPartialFill(_lotId);
+        assertEq(hasPartialFill, false, "hasPartialFill");
+        assertEq(partialFill.bidId, 0, "partialFill: bidId");
+        assertEq(partialFill.refund, 0, "partialFill: refund");
+        assertEq(partialFill.payout, 0, "partialFill: payout");
+    }
+
+    function test_bidsReachCapacity_quoteTokenDecimalsSmaller()
+        public
+        givenQuoteTokenDecimals(13)
+        givenBaseTokenDecimals(21)
+        givenLotIsCreated
+        givenLotHasStarted
+    {
+        // Create a bid to fill half capacity (10/2 = 5)
+        _createBid(_scaleQuoteTokenAmount(10e18));
+
+        // Create a second bid to fill the remaining capacity exactly
+        _createBid(_scaleQuoteTokenAmount(10e18));
+
+        // Assert state
+        IAuction.Lot memory lotData = _module.getLot(_lotId);
+        assertEq(lotData.capacity, _scaleBaseTokenAmount(_LOT_CAPACITY), "capacity");
+        assertEq(lotData.conclusion, uint48(block.timestamp), "conclusion");
+
+        IFixedPriceBatch.AuctionData memory auctionData = _module.getAuctionData(_lotId);
+        assertEq(uint8(auctionData.status), uint8(IFixedPriceBatch.LotStatus.Created), "status");
+        assertEq(auctionData.nextBidId, 3, "nextBidId");
+        assertEq(auctionData.settlementCleared, false, "settlementCleared"); // Not settled yet
+        assertEq(auctionData.totalBidAmount, _scaleQuoteTokenAmount(20e18), "totalBidAmount");
+
+        // Assert bid one
+        IFixedPriceBatch.Bid memory bidData = _module.getBid(_lotId, 1);
+        assertEq(bidData.bidder, _BIDDER, "bid one: bidder");
+        assertEq(bidData.referrer, _REFERRER, "bid one: referrer");
+        assertEq(bidData.amount, _scaleQuoteTokenAmount(10e18), "bid one: amount");
+        assertEq(
+            uint8(bidData.status), uint8(IFixedPriceBatch.BidStatus.Submitted), "bid one: status"
+        );
+
+        // Assert bid two
+        bidData = _module.getBid(_lotId, 2);
+        assertEq(bidData.bidder, _BIDDER, "bid two: bidder");
+        assertEq(bidData.referrer, _REFERRER, "bid two: referrer");
+        assertEq(bidData.amount, _scaleQuoteTokenAmount(10e18), "bid two: amount");
+        assertEq(
+            uint8(bidData.status), uint8(IFixedPriceBatch.BidStatus.Submitted), "bid two: status"
+        );
+
+        // Settle the auction
+        _warpAfterSettlePeriod();
+        _settleLot();
+
+        // Assert partial fill
+        (bool hasPartialFill, IFixedPriceBatch.PartialFill memory partialFill) =
+            _module.getPartialFill(_lotId);
+        assertEq(hasPartialFill, false, "hasPartialFill");
+        assertEq(partialFill.bidId, 0, "partialFill: bidId");
+        assertEq(partialFill.refund, 0, "partialFill: refund");
+        assertEq(partialFill.payout, 0, "partialFill: payout");
+    }
+
     function test_bidsOverCapacity() public givenLotIsCreated givenLotHasStarted {
         // Create a bid to fill half capacity (10/2 = 5)
         _createBid(10e18);
@@ -241,6 +355,116 @@ contract FpbBidTest is FpbTest {
         assertEq(partialFill.bidId, 2, "partialFill: bidId");
         assertEq(partialFill.refund, 2e18, "partialFill: refund");
         assertEq(partialFill.payout, 5e18, "partialFill: payout");
+    }
+
+    function test_bidsOverCapacity_quoteTokenDecimalsLarger()
+        public
+        givenQuoteTokenDecimals(21)
+        givenBaseTokenDecimals(13)
+        givenLotIsCreated
+        givenLotHasStarted
+    {
+        // Create a bid to fill half capacity (10/2 = 5)
+        _createBid(_scaleQuoteTokenAmount(10e18));
+
+        // Create a second bid to fill the over the remaining capacity (12/2 = 6)
+        _createBid(_scaleQuoteTokenAmount(12e18));
+
+        // Assert state
+        IAuction.Lot memory lotData = _module.getLot(_lotId);
+        assertEq(lotData.capacity, _scaleBaseTokenAmount(_LOT_CAPACITY), "capacity");
+        assertEq(lotData.conclusion, uint48(block.timestamp), "conclusion");
+
+        IFixedPriceBatch.AuctionData memory auctionData = _module.getAuctionData(_lotId);
+        assertEq(uint8(auctionData.status), uint8(IFixedPriceBatch.LotStatus.Created), "status");
+        assertEq(auctionData.nextBidId, 3, "nextBidId");
+        assertEq(auctionData.settlementCleared, false, "settlementCleared"); // Not settled yet
+        assertEq(auctionData.totalBidAmount, _scaleQuoteTokenAmount(20e18), "totalBidAmount"); // Excludes refund
+
+        // Assert bid one
+        IFixedPriceBatch.Bid memory bidData = _module.getBid(_lotId, 1);
+        assertEq(bidData.bidder, _BIDDER, "bid one: bidder");
+        assertEq(bidData.referrer, _REFERRER, "bid one: referrer");
+        assertEq(bidData.amount, _scaleQuoteTokenAmount(10e18), "bid one: amount");
+        assertEq(
+            uint8(bidData.status), uint8(IFixedPriceBatch.BidStatus.Submitted), "bid one: status"
+        );
+
+        // Assert bid two
+        bidData = _module.getBid(_lotId, 2);
+        assertEq(bidData.bidder, _BIDDER, "bid two: bidder");
+        assertEq(bidData.referrer, _REFERRER, "bid two: referrer");
+        assertEq(bidData.amount, _scaleQuoteTokenAmount(12e18), "bid two: amount");
+        assertEq(
+            uint8(bidData.status), uint8(IFixedPriceBatch.BidStatus.Submitted), "bid two: status"
+        );
+
+        // Settle the auction
+        _warpAfterSettlePeriod();
+        _settleLot();
+
+        // Assert partial fill
+        (bool hasPartialFill, IFixedPriceBatch.PartialFill memory partialFill) =
+            _module.getPartialFill(_lotId);
+        assertEq(hasPartialFill, true, "hasPartialFill");
+        assertEq(partialFill.bidId, 2, "partialFill: bidId");
+        assertEq(partialFill.refund, _scaleQuoteTokenAmount(2e18), "partialFill: refund");
+        assertEq(partialFill.payout, _scaleBaseTokenAmount(5e18), "partialFill: payout");
+    }
+
+    function test_bidsOverCapacity_quoteTokenDecimalsSmaller()
+        public
+        givenQuoteTokenDecimals(13)
+        givenBaseTokenDecimals(21)
+        givenLotIsCreated
+        givenLotHasStarted
+    {
+        // Create a bid to fill half capacity (10/2 = 5)
+        _createBid(_scaleQuoteTokenAmount(10e18));
+
+        // Create a second bid to fill the over the remaining capacity (12/2 = 6)
+        _createBid(_scaleQuoteTokenAmount(12e18));
+
+        // Assert state
+        IAuction.Lot memory lotData = _module.getLot(_lotId);
+        assertEq(lotData.capacity, _scaleBaseTokenAmount(_LOT_CAPACITY), "capacity");
+        assertEq(lotData.conclusion, uint48(block.timestamp), "conclusion");
+
+        IFixedPriceBatch.AuctionData memory auctionData = _module.getAuctionData(_lotId);
+        assertEq(uint8(auctionData.status), uint8(IFixedPriceBatch.LotStatus.Created), "status");
+        assertEq(auctionData.nextBidId, 3, "nextBidId");
+        assertEq(auctionData.settlementCleared, false, "settlementCleared"); // Not settled yet
+        assertEq(auctionData.totalBidAmount, _scaleQuoteTokenAmount(20e18), "totalBidAmount"); // Excludes refund
+
+        // Assert bid one
+        IFixedPriceBatch.Bid memory bidData = _module.getBid(_lotId, 1);
+        assertEq(bidData.bidder, _BIDDER, "bid one: bidder");
+        assertEq(bidData.referrer, _REFERRER, "bid one: referrer");
+        assertEq(bidData.amount, _scaleQuoteTokenAmount(10e18), "bid one: amount");
+        assertEq(
+            uint8(bidData.status), uint8(IFixedPriceBatch.BidStatus.Submitted), "bid one: status"
+        );
+
+        // Assert bid two
+        bidData = _module.getBid(_lotId, 2);
+        assertEq(bidData.bidder, _BIDDER, "bid two: bidder");
+        assertEq(bidData.referrer, _REFERRER, "bid two: referrer");
+        assertEq(bidData.amount, _scaleQuoteTokenAmount(12e18), "bid two: amount");
+        assertEq(
+            uint8(bidData.status), uint8(IFixedPriceBatch.BidStatus.Submitted), "bid two: status"
+        );
+
+        // Settle the auction
+        _warpAfterSettlePeriod();
+        _settleLot();
+
+        // Assert partial fill
+        (bool hasPartialFill, IFixedPriceBatch.PartialFill memory partialFill) =
+            _module.getPartialFill(_lotId);
+        assertEq(hasPartialFill, true, "hasPartialFill");
+        assertEq(partialFill.bidId, 2, "partialFill: bidId");
+        assertEq(partialFill.refund, _scaleQuoteTokenAmount(2e18), "partialFill: refund");
+        assertEq(partialFill.payout, _scaleBaseTokenAmount(5e18), "partialFill: payout");
     }
 
     function test_singleBidOverCapacity() public givenLotIsCreated givenLotHasStarted {
