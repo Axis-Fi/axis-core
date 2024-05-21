1c9e634af3d461f2c0210bad4d0de75f89aa8f6d
Oighty <oighty6@gmail.com>
2024-05-21 14:14:20 -0500

    chore: linter

--- test/callbacks/liquidity/BaselineV2/onCancel.t.sol
+++ test/callbacks/liquidity/BaselineV2/onCancel.t.sol
@@ -7,19 +7,19 @@ import {BaselineAxisLaunchTest} from
 import {BaseCallback} from "src/callbacks/BaseCallback.sol";
 
 contract BaselineOnCancelTest is BaselineAxisLaunchTest {
-    // ============ Modifiers ============ //
+// ============ Modifiers ============ //
 
-    // ============ Assertions ============ //
+// ============ Assertions ============ //
 
-    // ============ Tests ============ //
+// ============ Tests ============ //
 
-    // [ ] when the lot has not been registered
-    //  [ ] it reverts
-    // [ ] when the caller is not the auction house
-    //  [ ] it reverts
-    // [ ] when the lot has already been cancelled
-    //  [ ] it reverts
-    // [ ] when an insufficient quantity of the base token is transferred to the callback
-    //  [ ] it reverts
-    // [ ] it updates circulating supply, marks the auction as completed and burns the refunded base token quantity
+// [ ] when the lot has not been registered
+//  [ ] it reverts
+// [ ] when the caller is not the auction house
+//  [ ] it reverts
+// [ ] when the lot has already been cancelled
+//  [ ] it reverts
+// [ ] when an insufficient quantity of the base token is transferred to the callback
+//  [ ] it reverts
+// [ ] it updates circulating supply, marks the auction as completed and burns the refunded base token quantity
 }
--- test/callbacks/liquidity/BaselineV2/withdrawReserves.t.sol
+++ test/callbacks/liquidity/BaselineV2/withdrawReserves.t.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-import {BaselineAxisLaunchTest} from "test/callbacks/liquidity/BaselineV2/BaselineAxisLaunchTest.sol";
+import {BaselineAxisLaunchTest} from
+    "test/callbacks/liquidity/BaselineV2/BaselineAxisLaunchTest.sol";
 
 contract BaselineWithdrawReservesTest is BaselineAxisLaunchTest {
-    // ============ Tests ============ //
+// ============ Tests ============ //
 
-    // [ ] when the caller is not the owner
-    //  [ ] it reverts
-    // [ ] when there are no reserves
-    //  [ ] it returns 0
-    // [ ] it transfers the reserves to the owner
+// [ ] when the caller is not the owner
+//  [ ] it reverts
+// [ ] when there are no reserves
+//  [ ] it returns 0
+// [ ] it transfers the reserves to the owner
 }
