92d9432e489e14d2ed3598863bbb99ee1b89190d
Jem <0x0xjem@gmail.com>
2024-05-27 14:54:05 +0400

    Better documentation on merkle roots

--- test/callbacks/liquidity/BaselineV2/AllocatedAllowlist/onBid.t.sol
+++ test/callbacks/liquidity/BaselineV2/AllocatedAllowlist/onBid.t.sol
@@ -10,8 +10,10 @@ import {BALwithAllocatedAllowlist} from
 
 contract BaselineAllocatedAllowlistOnBidTest is BaselineAllocatedAllowlistTest {
     // Number values need to be converted to hex
-    // cast to-hex 5000000000000000000 = 0x4563918244f40000
-    // Then concatenated with the address and hashed with keccak256
+    // cast to-uint256 5000000000000000000 = 0x0000000000000000000000000000000000000000000000004563918244f40000
+    // Then concatenated with the address
+    // cast concat-hex "0x0000000000000000000000000000000000000004" "0x0000000000000000000000000000000000000000000000004563918244f40000"
+    // Then hashed with keccak256
     // cast keccak 0x00000000000000000000000000000000000000040000000000000000000000000000000000000000000000004563918244f40000 = 0x48371c09b043f1a28e5b80a2b295dcedc7e596fa1faacf355af6611b4d4eddc3
     // The hashed values can then be entered as leaves here: https://lab.miguelmota.com/merkletreejs/example/
 
