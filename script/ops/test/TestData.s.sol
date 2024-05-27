5ddc1d1a13c132ffc3ffe1792531079bad1df8b9
Jem <0x0xjem@gmail.com>
2024-05-27 14:57:13 +0400

    Add script for creating a FixedPriceBatch auction with BaselineAllocatedAllowlist callback

--- script/ops/test/TestData.s.sol
+++ script/ops/test/TestData.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 // Scripting libraries
 import {Script, console2} from "forge-std/Script.sol";
+import {WithEnvironment} from "script/deploy/WithEnvironment.s.sol";
 
 // System contracts
 import {BatchAuctionHouse} from "src/BatchAuctionHouse.sol";
@@ -10,11 +11,15 @@ import {IAuctionHouse} from "src/interfaces/IAuctionHouse.sol";
 import {toKeycode} from "src/modules/Modules.sol";
 import {EncryptedMarginalPrice} from "src/modules/auctions/batch/EMP.sol";
 import {Point} from "src/lib/ECIES.sol";
+import {ICallback} from "src/interfaces/ICallback.sol";
+import {IFixedPriceBatch} from "src/interfaces/modules/auctions/IFixedPriceBatch.sol";
+import {BaselineAxisLaunch} from "src/callbacks/liquidity/BaselineV2/BaselineAxisLaunch.sol";
+import {BALwithAllocatedAllowlist} from "src/callbacks/liquidity/BaselineV2/BALwithAllocatedAllowlist.sol";
 
 // Generic contracts
 import {MockERC20} from "lib/solmate/src/test/utils/mocks/MockERC20.sol";
 
-contract TestData is Script {
+contract TestData is Script, WithEnvironment {
     BatchAuctionHouse public auctionHouse;
     MockERC20 public quoteToken;
     MockERC20 public baseToken;
@@ -86,6 +91,62 @@ contract TestData is Script {
         uint96 lotId = auctionHouse.auction(routingParams, auctionParams, infoHash);
 
         vm.stopBroadcast();
+
+        return lotId;
+    }
+
+    function createBaselineFixedPriceBatchAuction(
+        string calldata chain_,
+        address quoteToken_,
+        address baseToken_,
+        address callback_,
+        bytes32 merkleRoot
+    ) public returns (uint96) {
+        // Load addresses from .env
+        _loadEnv(chain_);
+        auctionHouse = BatchAuctionHouse(_envAddressNotZero("axis.BatchAuctionHouse"));
+
+        vm.startBroadcast();
+
+        // No spending approval necessary, since the callback will handle it
+
+        // Create Fixed Price Batch auction
+        IAuctionHouse.RoutingParams memory routingParams;
+        routingParams.auctionType = toKeycode("FPBA");
+        routingParams.baseToken = baseToken_;
+        routingParams.quoteToken = quoteToken_;
+        routingParams.callbacks = ICallback(callback_);
+        routingParams.callbackData = abi.encode(BaselineAxisLaunch.CreateData({
+            discoveryTickWidth: 100,
+            allowlistParams: abi.encode(
+                BALwithAllocatedAllowlist.AllocatedAllowlistCreateParams({
+                    merkleRoot: merkleRoot
+                })
+            )
+        }));
+
+        IFixedPriceBatch.AuctionDataParams memory auctionDataParams;
+        auctionDataParams.price = 1e18; // 1 quote tokens per base token
+        auctionDataParams.minFillPercent = uint24(10_000); // 10%
+        bytes memory implParams = abi.encode(auctionDataParams);
+
+        uint48 duration = 86_400; // 1 day
+
+        IFixedPriceBatch.AuctionParams memory auctionParams;
+        auctionParams.start = uint48(0); // immediately
+        auctionParams.duration = duration;
+        // capaity is in base token
+        auctionParams.capacity = 10e18; // 10 base tokens
+        auctionParams.implParams = implParams;
+
+        string memory infoHash = "";
+
+        uint96 lotId = auctionHouse.auction(routingParams, auctionParams, infoHash);
+
+        vm.stopBroadcast();
+
+        console2.log("Fixed Price Batch auction created with lot ID: ", lotId);
+        console2.log("Auction ends at timestamp", block.timestamp + duration);
 
         return lotId;
     }
