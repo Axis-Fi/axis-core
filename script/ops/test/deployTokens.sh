77db7edcdb45be4c0f7334ac26338a8a8b23c6a7
Jem <0x0xjem@gmail.com>
2024-05-29 17:35:07 +0400

    Add logs to deployTokens script

--- script/ops/test/deployTokens.sh
+++ script/ops/test/deployTokens.sh
@@ -3,6 +3,11 @@
 # Load environment variables
 source .env
 
+echo "RPC: $RPC_URL"
+echo "Deployer: $DEPLOYER_ADDRESS"
+echo "Seller: $1"
+echo "Buyer: $2"
+
 # Create auction
 forge script ./script/ops/test/TestData.s.sol:TestData --sig "deployTestTokens(address,address)()" $1 $2 \
 --rpc-url $RPC_URL --private-key $DEPLOYER_PRIVATE_KEY --froms $DEPLOYER_ADDRESS --slow -vvv \
