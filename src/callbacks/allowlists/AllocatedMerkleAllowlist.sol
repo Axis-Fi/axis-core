90f170cd5cff77a94822132a1296aaf6691312ad
Oighty <oighty6@gmail.com>
2024-05-30 21:39:33 -0500

    refactor: allocated allowlist to inherit standard allowlist

--- src/callbacks/allowlists/AllocatedMerkleAllowlist.sol
+++ src/callbacks/allowlists/AllocatedMerkleAllowlist.sol
@@ -3,26 +3,16 @@ pragma solidity 0.8.19;
 
 import {MerkleProofLib} from "lib/solady/src/utils/MerkleProofLib.sol";
 
-import {BaseCallback} from "src/callbacks/BaseCallback.sol";
+import {MerkleAllowlist} from "src/callbacks/allowlists/MerkleAllowlist.sol";
 import {Callbacks} from "src/lib/Callbacks.sol";
 
-import {IAuctionHouse} from "src/interfaces/IAuctionHouse.sol";
-
-contract AllocatedMerkleAllowlist is BaseCallback {
+contract AllocatedMerkleAllowlist is MerkleAllowlist {
     // ========== ERRORS ========== //
 
     /// @notice Error message when the bid amount exceeds the limit assigned to a buyer
     error Callback_ExceedsLimit();
 
-    // ========== EVENTS ========== //
-
-    /// @notice Emitted when the merkle root is set
-    event MerkleRootSet(uint96 lotId, bytes32 merkleRoot);
-
     // ========== STATE VARIABLES ========== //
-
-    /// @notice The root of the merkle tree that represents the allowlist for a lot
-    mapping(uint96 lotId => bytes32 merkleRoot) public lotMerkleRoot;
 
     /// @notice Tracks the cumulative amount spent by a buyer on a lot
     mapping(uint96 lotId => mapping(address buyer => uint256 spent)) public lotBuyerSpent;
@@ -43,40 +33,9 @@ contract AllocatedMerkleAllowlist is BaseCallback {
     constructor(
         address auctionHouse_,
         Callbacks.Permissions memory permissions_
-    ) BaseCallback(auctionHouse_, permissions_) {}
+    ) MerkleAllowlist(auctionHouse_, permissions_) {}
 
     // ========== CALLBACK FUNCTIONS ========== //
-
-    function _onCreate(
-        uint96 lotId_,
-        address,
-        address,
-        address,
-        uint256,
-        bool,
-        bytes calldata callbackData_
-    ) internal virtual override {
-        // Check that the parameters are of the correct length
-        if (callbackData_.length != 32) {
-            revert Callback_InvalidParams();
-        }
-
-        // Decode the merkle root from the callback data
-        bytes32 merkleRoot = abi.decode(callbackData_, (bytes32));
-
-        // Set the merkle root
-        lotMerkleRoot[lotId_] = merkleRoot;
-    }
-
-    function _onCancel(uint96, uint256, bool, bytes calldata) internal pure override {
-        // Not implemented
-        revert Callback_NotImplemented();
-    }
-
-    function _onCurate(uint96, uint256, bool, bytes calldata) internal pure override {
-        // Not implemented
-        revert Callback_NotImplemented();
-    }
 
     function _onPurchase(
         uint96 lotId_,
@@ -85,7 +44,7 @@ contract AllocatedMerkleAllowlist is BaseCallback {
         uint256,
         bool,
         bytes calldata callbackData_
-    ) internal virtual override {
+    ) internal override {
         _onBuy(lotId_, buyer_, amount_, callbackData_);
     }
 
@@ -95,9 +54,11 @@ contract AllocatedMerkleAllowlist is BaseCallback {
         address buyer_,
         uint256 amount_,
         bytes calldata callbackData_
-    ) internal virtual override {
+    ) internal override {
         _onBuy(lotId_, buyer_, amount_, callbackData_);
     }
+
+    // ========== INTERNAL FUNCTIONS ========== //
 
     function _onBuy(
         uint96 lotId_,
@@ -105,28 +66,8 @@ contract AllocatedMerkleAllowlist is BaseCallback {
         uint256 amount_,
         bytes calldata callbackData_
     ) internal {
-        // Validate that the buyer is allowed to participate and get their allocated amount
-        uint256 allocatedAmount = _canParticipate(lotId_, buyer_, callbackData_);
-
-        // Validate that the buyer can spend the amount
-        _canBuy(lotId_, buyer_, amount_, allocatedAmount);
-
-        // Update the buyer's spent amount
-        lotBuyerSpent[lotId_][buyer_] += amount_;
-    }
-
-    function _onSettle(uint96, uint256, uint256, bytes calldata) internal pure override {
-        // Not implemented
-        revert Callback_NotImplemented();
-    }
-
-    // ========== INTERNAL FUNCTIONS ========== //
-
-    function _canParticipate(
-        uint96 lotId_,
-        address buyer_,
-        bytes calldata callbackData_
-    ) internal view returns (uint256) {
+        // Validate that the buyer is allowed to participate
+        
         // Decode the merkle proof and allocated amount from buyer submitted callback data
         (bytes32[] memory proof, uint256 allocatedAmount) =
             abi.decode(callbackData_, (bytes32[], uint256));
@@ -139,49 +80,13 @@ contract AllocatedMerkleAllowlist is BaseCallback {
             revert Callback_NotAuthorized();
         }
 
-        // Return the allocated amount
-        return allocatedAmount;
-    }
-
-    function _canBuy(
-        uint96 lotId_,
-        address buyer_,
-        uint256 amount_,
-        uint256 allocatedAmount_
-    ) internal view {
-        // Check if the buyer has already spent their limit
-        if (lotBuyerSpent[lotId_][buyer_] + amount_ > allocatedAmount_) {
+        // Validate that the buyer can spend the amount
+        // They cannot spend over the allocated amount
+        if (lotBuyerSpent[lotId_][buyer_] + amount_ > allocatedAmount) {
             revert Callback_ExceedsLimit();
         }
-    }
 
-    // ========== ADMIN FUNCTIONS ========== //
-
-    /// @notice Sets the merkle root for the allowlist
-    ///         This function can be called by the owner to update the merkle root after `onCreate()`.
-    /// @dev    This function performs the following:
-    ///         - Performs validation
-    ///         - Sets the merkle root
-    ///         - Emits a MerkleRootSet event
-    ///
-    ///         This function reverts if:
-    ///         - The caller is not the lot's owner
-    ///         - The auction has not been registered
-    ///         - The auction has been completed
-    ///
-    /// @param  merkleRoot_ The new merkle root
-    function setMerkleRoot(uint96 lotId_, bytes32 merkleRoot_) external onlyRegisteredLot(lotId_) {
-        // We check that the lot is registered on this callback
-
-        // Check that the caller is the lot's owner
-        (address seller,,,,,,,,) = IAuctionHouse(AUCTION_HOUSE).lotRouting(lotId_);
-        if (msg.sender != seller) {
-            revert Callback_NotAuthorized();
-        }
-
-        // Set the new merkle root and emit an event
-        lotMerkleRoot[lotId_] = merkleRoot_;
-
-        emit MerkleRootSet(lotId_, merkleRoot_);
+        // Update the buyer's spent amount
+        lotBuyerSpent[lotId_][buyer_] += amount_;
     }
 }
