19ee0d7c8c93307f0316982d3fdc7b3eb5cb948a
Jem <0x0xjem@gmail.com>
2024-07-16 13:18:10 +0400

    Shift callbacks to axis-periphery. Remove git subtrees used by callbacks.

--- test/Constants.sol
+++ test/Constants.sol
@@ -4,16 +4,4 @@ pragma solidity 0.8.19;
 abstract contract TestConstants {
     address internal constant _OWNER = address(0x1);
     address internal constant _AUCTION_HOUSE = address(0x000000000000000000000000000000000000000A);
-    address internal constant _UNISWAP_V2_FACTORY =
-        address(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
-    address internal constant _UNISWAP_V2_ROUTER =
-        address(0xAA060467e63D0796E8Ef8dc030E9e41062F640Cf);
-    address internal constant _UNISWAP_V3_FACTORY =
-        address(0xAA87607802bF920dF9456cfE2a39A0545844ABc6);
-    address internal constant _GUNI_FACTORY = address(0xAAa7f66D711841D06a5ac0ACC454b9893f553738);
-    address internal constant _BASELINE_KERNEL = address(0xBB);
-    address internal constant _BASELINE_QUOTE_TOKEN =
-        address(0xAA58516d932C482469914260268EEA7611BF0eb4);
-    address internal constant _CREATE2_DEPLOYER =
-        address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
 }
