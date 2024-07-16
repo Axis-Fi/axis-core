dbde451a0975282a29a62a4d4be21916f87b197c
Jem <0x0xjem@gmail.com>
2024-07-16 16:11:27 +0400

    Update docs. Remove expectation of soldeer CLI.

--- script/publish.sh
+++ script/publish.sh
@@ -18,15 +18,8 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# Check that soldeer is present
-if ! command -v soldeer &> /dev/null
-then
-    echo "soldeer could not be found. Please install it by running 'cargo install soldeer'."
-    exit
-fi
-
 # Update the version number in the package.json file
 npm version $1
 
 # Push the new version to soldeer
-soldeer push axis-core~v$1
+forge soldeer push axis-core~v$1
