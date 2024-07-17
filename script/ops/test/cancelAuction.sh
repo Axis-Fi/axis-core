cef9e84a9cbe22a673a1985cc0e341c3f770a819
Jem <0x0xjem@gmail.com>
2024-07-17 20:17:05 +0400

    Remove test scripts moved to axis-utils

--- script/ops/test/cancelAuction.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Load environment variables
-source .env
-
-# Cancel auction
-forge script ./script/ops/test/TestData.s.sol:TestData --sig "cancelAuction(uint96)()" $1 \
---rpc-url $RPC_URL --private-key $DEPLOYER_PRIVATE_KEY --froms $DEPLOYER_ADDRESS --slow -vvv \
---broadcast
