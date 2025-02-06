0bc55ad3e7b3d5d49041aa62b36bf5cc4762e2be
Oighty <oighty6@gmail.com>
2025-02-06 09:01:51 -0600

    deploy: berachain

--- src/modules/auctions/batch/FPB.sol
+++ src/modules/auctions/batch/FPB.sol
@@ -46,7 +46,7 @@ contract FixedPriceBatch is BatchAuctionModule, IFixedPriceBatch {
 
     /// @inheritdoc Module
     function VEECODE() public pure override returns (Veecode) {
-        return toVeecode("03FPBA");
+        return toVeecode("01FPBA");
     }
 
     // ========== AUCTION ========== //
